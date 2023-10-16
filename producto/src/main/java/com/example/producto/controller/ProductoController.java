5c6b542aea27c119dd10536d955c5c7ea6cd53b0
JOSELIN-CONDORI <yoselin.tintaya.25@gmail.com>
2023-10-15 22:37:40 -0500

    agregando resilencia

--- producto/src/main/java/com/example/producto/controller/ProductoController.java
+++ producto/src/main/java/com/example/producto/controller/ProductoController.java
@@ -1,14 +1,14 @@
 package com.example.producto.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.producto.entity.Producto;
 import com.example.producto.service.ProductoService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 import java.util.List;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 @RestController
@@ -32,14 +32,29 @@ public class ProductoController {
         return ResponseEntity.ok(productoService.actualizar(producto));
     }
 
+    @CircuitBreaker(name = "listByIdCB", fallbackMethod = "fallBacklistById")
     @GetMapping("/{id}")
     public ResponseEntity<Producto> listById(@PathVariable(required = true) Integer id) {
         return ResponseEntity.ok().body(productoService.listarPorId(id).get());
     }
 
+    @CircuitBreaker(name = "deleteByIdCB", fallbackMethod = "fallBackDeleteById")
     @DeleteMapping("/{id}")
     public String deleteById(@PathVariable(required = true) Integer id) {
         productoService.eliminarPorId(id);
         return "Eliminacion Correcta";
+    }
+
+    // resilencia
+    private ResponseEntity<Producto> fallBacklistById(@PathVariable(required = true) Integer id, RuntimeException e) {
+        Producto producto = new Producto();
+        producto.setId(90000);
+        producto.setNombre("Recurso no disponible Nombre");
+        return ResponseEntity.ok().body(producto);
+    }
+
+    private ResponseEntity<String> fallBackDeleteById(@PathVariable(required = true) Integer id, RuntimeException e) {
+        // Aquí puedes devolver una respuesta alternativa en caso de error, por ejemplo:
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar el registro.");
     }
 }
--- venta/src/main/java/com/example/venta/controller/VentaController.java
+++ venta/src/main/java/com/example/venta/controller/VentaController.java
@@ -1,10 +1,16 @@
 package com.example.venta.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import com.example.venta.dto.Cliente;
 import com.example.venta.entity.Venta;
 import com.example.venta.service.VentaService;
+import java.util.Collections;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+
 import java.util.List;
 
 @RestController
@@ -28,14 +34,37 @@ public class VentaController {
         return ResponseEntity.ok(ventaService.actualizar(venta));
     }
 
+    @CircuitBreaker(name = "listByIdCB", fallbackMethod = "fallBacklistById")
     @GetMapping("/{id}")
     public ResponseEntity<Venta> listById(@PathVariable(required = true) Integer id) {
         return ResponseEntity.ok().body(ventaService.listarPorId(id).get());
     }
 
+    @CircuitBreaker(name = "deleteByIdCB", fallbackMethod = "fallBackDeleteById")
     @DeleteMapping("/{id}")
     public String deleteById(@PathVariable(required = true) Integer id) {
         ventaService.eliminarPorId(id);
         return "Eliminacion Correcta";
     }
+
+    // resilencia
+    private ResponseEntity<Venta> fallBacklistById(@PathVariable(required = true) Integer id, RuntimeException e) {
+        Venta venta = new Venta();
+        venta.setId(90000);
+        Cliente cliente = new Cliente();
+        cliente.setNombre("Recurso no disponible del nombre del cliente");
+        cliente.setApellidomaterno("no tiene Apellidomaterno");
+        cliente.setCelular("no tiene Celular");
+        cliente.setDni("no tiene Dni");
+        cliente.setTelefono("no tiene Telefono");
+
+        venta.setCliente(cliente);
+        return ResponseEntity.ok().body(venta);
+    }
+
+    private ResponseEntity<String> fallBackDeleteById(@PathVariable(required = true) Integer id, RuntimeException e) {
+        // Aquí puedes devolver una respuesta alternativa en caso de error, por ejemplo:
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error al eliminar el registro.");
+    }
+
 }
